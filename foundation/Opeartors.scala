7cd922eb52636a3b4a7a702ad0f2fa2407c4ea20
Victor Hugo <hugo.victoor77@gmail.com>
2025-06-18 07:57:29 -0300

    Result Types

--- foundation/Opeartors.scala
+++ foundation/Opeartors.scala
@@ -9,4 +9,27 @@ def operators(): Unit = {
     println(7 % 2) // Module or remainder (get the rest of the / operation)
     println(15 % 6)
 
+
+
+    // Result Type gets always the larges type
+    val a: Short = 4
+    val b: Int = 372
+    val r = a * b
+
+    println(r) // 1488
+    println(r.getClass()) // int
+
+    val a2 = 9897878789l
+    val b2 = 232.4578f
+    val r2 = a2 / b2
+
+    println(r2) // 4.257925E7
+    println(r2.getClass()) //float
+
+    val a3 = 23234.4323f
+    val b3 = 23.3555 // double by default
+    val r3 = a3 * b3
+
+    println(r3)
+    println(r3.getClass()) // double
 }
