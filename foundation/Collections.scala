f68a491daca7a154293eb8a9483e4fb1ff32d09f
Victor Hugo <hugo.victoor77@gmail.com>
2025-06-23 08:19:54 -0300

    Set

--- foundation/Collections.scala
+++ foundation/Collections.scala
@@ -1,10 +1,15 @@
 @main
 def collections(): Unit = {
     // List is a oredred collectiong of elements
-    // can be accessed by the position (index)
+    // can be accessed by the position (index), can contain duplicate elements
     val a = List(1, 2, 3, 632, 9, 52, 6)
     val b = List(1, "hello", true)
     val c = List()
 
-    // Mutable vs Immutable 
+    // Set
+    // group of unique elements
+    // the order has no significance
+    // we can iterate through it but ther order is not defined
+    Set(1, 2, 3, 632, 9, 52, 6)
+    // a deck of card is a set
 }
