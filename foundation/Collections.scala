6afe9ee3165ec4e54b94ded8968fc820b2665285
Victor Hugo <hugo.victoor77@gmail.com>
2025-06-23 08:17:04 -0300

    Collections - List

---
+++ foundation/Collections.scala
@@ -0,0 +1,10 @@
+@main
+def collections(): Unit = {
+    // List is a oredred collectiong of elements
+    // can be accessed by the position (index)
+    val a = List(1, 2, 3, 632, 9, 52, 6)
+    val b = List(1, "hello", true)
+    val c = List()
+
+    // Mutable vs Immutable 
+}
