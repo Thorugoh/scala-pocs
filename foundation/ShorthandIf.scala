7495a26204c9b34cfe295da87273d04b3115e033
Victor Hugo <hugo.victoor77@gmail.com>
2025-06-27 08:33:24 -0300

    shortHandIf

---
+++ foundation/ShorthandIf.scala
@@ -0,0 +1,11 @@
+import scala.io.StdIn
+@main
+def shorthandIf(): Unit = {
+    println("Please enter a number")
+    val number = StdIn.readInt()
+    var isEven: String = null
+
+    isEven = if(number % 2 == 0) "Number is even" else "Number is odd"
+
+    println(isEven)
+}
