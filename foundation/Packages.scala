8d64da9ccb1514a150e53f843961c9f0ee8f1860
Victor Hugo <hugo.victoor77@gmail.com>
2025-07-16 09:05:59 -0300

    Packages definition

--- foundation/Packages.scala
+++ foundation/Packages.scala
@@ -1,7 +1,27 @@
 import scala.util.Random
 
+// A package is a way to group related code together
+// Files, functions and classes are part of packages
+
+// package admin:
+// typically packages follow directory structure
+// scope (visibility) within the same package
+// In order to use code from another package, that package must be imported into the project
+
 @main
 def packages(): Unit = {
     val abc = List("a", "b", "c")
     val randNb = Random.nextInt()
+
+    // since its on same file there is no need to import
+    admin.doAdmin()
+    hr.doHr()
 }
+
+package admin:
+    def doAdmin() = 
+        println("Doing admin")
+
+package hr:
+    def doHr() =
+        println("Doing Hr")
