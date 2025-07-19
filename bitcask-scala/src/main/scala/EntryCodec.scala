5e0e3c0c53c08804037aed4188c0cdd27a6fda24
Victor Hugo <hugo.victoor77@gmail.com>
2025-07-19 11:55:02 -0300

    feat: entry header and codec

--- bitcask-scala/src/main/scala/EntryCodec.scala
+++ bitcask-scala/src/main/scala/EntryCodec.scala
@@ -1,1 +1,27 @@
-class EntryCodec
+case class Entry(header: EntryHeader, key: String, value: Array[Byte])
+
+object EntryCodec:
+  import BytePacking.*
+  import ByteDecoder.*
+
+  def encodeEntry(key: String, value: Array[Byte], timestamp: Int, crc: Int = 0): Array[Byte] =
+    val keyBytes: Array[Byte] = key.toBytes
+    val header = EntryHeader(
+      crc = crc,
+      timestamp = timestamp,
+      keySize = keyBytes.length.toShort,
+      valueSize = value.length
+    )
+    val encodedHeader = EntryHeader.encode(header)
+    pack(encodedHeader, keyBytes, value)
+
+  def decodeEntry(bytes: Array[Byte]): Entry =
+    val header = EntryHeader.decode(bytes.slice(0, 14))
+    val keyStart = 14
+    val keyEnd = keyStart + header.keySize
+    val valueEnd = keyEnd + header.valueSize
+
+    val keyBytes = bytes.slice(keyStart, keyEnd)
+    val valueBytes = bytes.slice(keyEnd, valueEnd)
+
+    Entry(header, keyBytes.toUtf8String, valueBytes)
--- bitcask-scala/src/main/scala/EntryHeader.scala
+++ bitcask-scala/src/main/scala/EntryHeader.scala
@@ -1,1 +1,41 @@
-class EntryHeader
+
+val CRC_SIZE = 4
+val TIMESTAMP_SIZE = 4
+val KEY_SIZE = 2
+val VALUE_SIZE = 4
+
+case class EntryHeader(
+  crc: Int,
+  timestamp: Int,
+  keySize: Short,
+  valueSize: Int
+) {
+
+  import EntryHeader.HeaderSize
+
+  def totalSize: Int = HeaderSize + keySize + valueSize
+}
+
+
+object EntryHeader {
+  import BytePacking.*
+  import ByteDecoder.*
+
+  private val HeaderSize: Int = CRC_SIZE + TIMESTAMP_SIZE + KEY_SIZE + VALUE_SIZE
+
+  def encode(header: EntryHeader): Array[Byte] =
+    BytePacking.pack(
+      header.crc.toBytesLE,
+      header.timestamp.toBytesLE,
+      header.keySize.toBytesLE,
+      header.valueSize.toBytesLE
+    )
+
+  def decode(bytes: Array[Byte]): EntryHeader =
+    val crc = bytes.slice(0, 4).toIntLe
+    val timestamp = bytes.slice(4, 8).toIntLe
+    val keySize = bytes.slice(8, 10).toShortLE
+    val valueSize = bytes.slice(10, 14).toIntLe
+
+    EntryHeader(crc,timestamp, keySize, valueSize)
+}
