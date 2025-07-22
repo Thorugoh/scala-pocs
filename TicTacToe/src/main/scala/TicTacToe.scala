4a773bbc09bdc49ab86e51faf25f8d6d4bf64516
Victor Hugo <hugo.victoor77@gmail.com>
2025-07-22 19:01:32 -0300

    handle players move

--- TicTacToe/src/main/scala/TicTacToe.scala
+++ TicTacToe/src/main/scala/TicTacToe.scala
@@ -1,4 +1,5 @@
 import scala.collection.mutable.ArrayBuffer
+import scala.io.StdIn
 
 var board = ArrayBuffer[ArrayBuffer[String]]()
 
@@ -11,9 +12,27 @@ def ticTacToe() =
     }
     board.addOne(row)
   }
-  board(1)(1) = "X"
-  board(2)(1) = "O"
   printBoard()
+
+  var continueGame = true
+  while(continueGame)
+    println("Enter a position (e.g. 1,1)")
+    val input = StdIn.readLine()
+    var x = 0
+    var y = 0
+    try
+        val position = input.split(",")
+        x = position(0).trim.toInt
+        y = position(1).trim.toInt
+
+        if(board(x-1)(y-1) != "")
+          println("Position already taken, try again")
+        else
+          board(x-1)(y-1) = "X"
+          printBoard()
+
+    catch
+      case e: Exception => println("Invalid input, try again.")
 
 def printBoard() =
   println("-------------")
