5c0aea00fc3327c719e12b50b419a9bca8dafb53
Victor Hugo <hugo.victoor77@gmail.com>
2025-07-31 20:11:43 -0300

    class methods

--- foundation/Classes.scala
+++ foundation/Classes.scala
@@ -1,3 +1,9 @@
 class Car:
     var brand: String = "Toyota"
     var topSpeed = 100
+
+    def start() =
+        println(s"Starting the $brand")
+
+    def drive(speed: Int) =
+        println(s"Driving at speed of $speed")
