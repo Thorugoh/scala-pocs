ffb15838af736c75f8aa0bb179d59c5d6127b58c
Victor Hugo <hugo.victoor77@gmail.com>
2025-08-27 19:39:21 -0300

    Add two threads to the pool

--- ExecutionContext/src/main/scala/SimpleExecutionContext.scala
+++ ExecutionContext/src/main/scala/SimpleExecutionContext.scala
@@ -1,22 +1,38 @@
 import java.time.LocalDateTime
-import java.util.concurrent.Executors
-import scala.concurrent.duration._
+import java.util.concurrent.{Executors, ThreadFactory}
+import scala.concurrent.duration.*
 import scala.concurrent.{Await, ExecutionContext, Future}
 
 object SimpleExecutionContext {
-  private val executorService = Executors.newFixedThreadPool(1)
-  private val ec = ExecutionContext.fromExecutor(executorService)
+  private val executorService = Executors.newFixedThreadPool(2)
+  implicit private val ec: ExecutionContext = ExecutionContext.fromExecutor(executorService)
 
   def main(args: Array[String]): Unit = {
     printWithThreadName("Application has been started.")
-    val f = executeComputeIntensiveTask()
-    Await.result(f, 1.seconds)
+    val f1 = executeComputeIntensiveTaskA()
+    val f2 = executeComputeIntensiveTaskB()
+
+    val result  = for {
+      _ <- f1
+      _ <- f2
+    } yield()
+
+    Await.result(result, 5.seconds)
+    executorService.shutdown()
   }
 
-  private def executeComputeIntensiveTask(): Future[Unit] = {
+  private def executeComputeIntensiveTaskA(): Future[Unit] = {
     Future {
-      printWithThreadName("Task has been executed.")
-    }(ec)
+      printWithThreadName("Task-A has been started.")
+      Thread.sleep(3000)
+    }
+  }
+
+  private def executeComputeIntensiveTaskB(): Future[Unit] = {
+    Future {
+      printWithThreadName("Task-B has been executed.")
+      Thread.sleep(1000)
+    }
   }
 
   private def printWithThreadName(message: String): Unit = {
