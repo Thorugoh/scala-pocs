6aebe6475e534893fa0e6c43c98ecc6198317fda
Victor Hugo <hugo.victoor77@gmail.com>
2026-02-26 10:41:44 -0300

    initialize SpringApplication

--- GameStore/src/main/scala/com/gamestore/GameStoreApplication.scala
+++ GameStore/src/main/scala/com/gamestore/GameStoreApplication.scala
@@ -1,1 +1,13 @@
+package com.gamestore
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+
+
+@SpringBootApplication
 class GameStoreApplication
+
+object GameStoreApplication
+def main(args: Array[String]): Unit =
+    SpringApplication.run(classOf[GameStoreApplication], args*)
