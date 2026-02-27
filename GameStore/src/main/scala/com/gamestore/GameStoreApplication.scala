f3eada46df5c9bfc13bc109163cdb9443299f75a
Victor Hugo <hugo.victoor77@gmail.com>
2026-02-27 10:23:55 -0300

    Configure Jackson object mapper

--- GameStore/src/main/scala/com/gamestore/GameStoreApplication.scala
+++ GameStore/src/main/scala/com/gamestore/GameStoreApplication.scala
@@ -1,13 +1,22 @@
 package com.gamestore
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
 
 
 
 @SpringBootApplication
 class GameStoreApplication
 
-object GameStoreApplication
-def main(args: Array[String]): Unit =
-    SpringApplication.run(classOf[GameStoreApplication], args*)
+object GameStoreApplication:
+    def main(args: Array[String]): Unit =
+        SpringApplication.run(classOf[GameStoreApplication], args*)
+
+    @Bean
+    def objectMapper(): ObjectMapper =
+        val mapper = ObjectMapper()
+        mapper.registerModule(DefaultScalaModule)
+        mapper
