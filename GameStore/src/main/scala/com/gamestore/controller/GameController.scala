e5396eec8ceda47db85cc586f918f1c0fedf83ab
Victor Hugo <hugo.victoor77@gmail.com>
2026-02-27 10:23:14 -0300

    use java list to map game controller

--- GameStore/src/main/scala/com/gamestore/controller/GameController.scala
+++ GameStore/src/main/scala/com/gamestore/controller/GameController.scala
@@ -3,16 +3,15 @@ package com.gamestore.controller
 import com.gamestore.domain.Game
 import com.gamestore.repository.GameRepository
 import org.springframework.web.bind.annotation.{GetMapping, PostMapping, RequestBody, RequestMapping, RestController}
-import scala.jdk.CollectionConverters.*
 
 @RestController
 @RequestMapping(Array("/api/games"))
 class GameController(repository: GameRepository):
-  
+
   @GetMapping
-  def listAll(): List[Game] =
-    repository.findAll().asScala.toList
-    
+  def listAll(): java.util.List[Game] =
+    repository.findAll()
+
   @PostMapping
   def create(@RequestBody game: Game): Game =
     repository.save(game)
