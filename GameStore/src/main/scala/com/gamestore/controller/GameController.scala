c6989405cce991e9cf6eac0fe75720756ffa53a9
Victor Hugo <hugo.victoor77@gmail.com>
2026-02-27 09:59:42 -0300

    Add controller

--- GameStore/src/main/scala/com/gamestore/controller/GameController.scala
+++ GameStore/src/main/scala/com/gamestore/controller/GameController.scala
@@ -1,3 +1,19 @@
 package com.gamestore.controller
 
-case class GameController()
+import com.gamestore.domain.Game
+import com.gamestore.repository.GameRepository
+import org.springframework.web.bind.annotation.{GetMapping, PostMapping, RequestBody, RequestMapping, RestController}
+import scala.jdk.CollectionConverters.*
+
+@RestController
+@RequestMapping(Array("/api/games"))
+class GameController(repository: GameRepository):
+  
+  @GetMapping
+  def listAll(): List[Game] =
+    repository.findAll().asScala.toList
+    
+  @PostMapping
+  def create(@RequestBody game: Game): Game =
+    repository.save(game)
+
