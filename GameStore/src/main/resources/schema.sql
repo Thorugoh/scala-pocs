289de6b4339058500a777be79d82f2c1660f1577
Victor Hugo <hugo.victoor77@gmail.com>
2026-02-27 10:24:31 -0300

    use UUID instead of serial on schema

--- GameStore/src/main/resources/schema.sql
+++ GameStore/src/main/resources/schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS games (
-    id UUID PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     title VARCHAR(255) NOT NULL,
-    price  DECIMAL(10, 2) NOT NULL,
-)
+    price  DECIMAL(10, 2) NOT NULL
+);
